f3f79eeac6c1b54b1da20ac4dc0ef09a72054c5b
Davide Piscia <dpiscia@Davides-MacBook-Air.local>
2017-03-03 14:20:52 +0100

    chrom in load parser

--- src/main/scala/GenomicsLoader.scala
+++ src/main/scala/GenomicsLoader.scala
@@ -119,7 +119,7 @@ object GenomicsLoader {
 
         var rawData = sqlContext.load(loaded+"/loaded")
         for (band <- due) yield {
-          steps.Parser.main(sqlContext, rawData, destination + "/parsedSamples",ch, band,repartitions)
+          steps.Parser.main(sqlContext, rawData, destination + "/parsedSamples/chrom="+ch,ch, band,repartitions)
         }
       }
       if (pipeline.contains("umd.get")) {

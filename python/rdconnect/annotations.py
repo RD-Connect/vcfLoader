5f9deb386b3982d67641767d7db535b8b4c28e29
Davide Piscia <dpiscia@Davides-MacBook-Air.local>
2017-10-10 08:35:11 +0200

    rename fields

--- python/rdconnect/annotations.py
+++ python/rdconnect/annotations.py
@@ -13,7 +13,7 @@ def dbnsfpTAble(hc,sourcePath,destinationPath):
     print("source Path is "+sourcePath)
     dbnsfpTable=hc.import_table(sourcePath).annotate('variant= Variant(`#chr`,`pos(1-coor)`.toInt,`ref`,`alt`)').key_by('variant')
     #select(['C3', 'C1', 'C2']) select which column we are interested or drop
-    dbnsfpTable.rename({'1000Gp1_EUR_AF':'Gp1_EUR_AF1000','1000Gp1_ASN_AF':'Gp1_ASN_AF1000','1000Gp1_AFR_AF':'Gp1_AFR_AF1000'}).write(destinationPath,overwrite=True)
+    dbnsfpTable.rename({'1000Gp1_EUR_AF':'Gp1_EUR_AF1000','1000Gp1_ASN_AF':'Gp1_ASN_AF1000','1000Gp1_AFR_AF':'Gp1_AFR_AF1000','ESP6500_EA_AF ':'ESP6500_EA_AF'}).write(destinationPath,overwrite=True)
 
 
 def annotatedbnsfp(hc,variants, dbsfp_path,destinationPath):

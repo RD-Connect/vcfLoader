829ca7b96f66eb4989c21074cf36f1716a86c5a5
Davide Piscia <dpiscia@Davides-MacBook-Air.local>
2017-10-17 12:38:56 +0200

    partitions

--- python/rdconnect/loadVCF.py
+++ python/rdconnect/loadVCF.py
@@ -7,7 +7,7 @@ def importVCF(hc,source_path, destination_path,number_partitions):
         print ("reading vcf from "+ source_path )
         vcf= hc.import_vcf(str(source_path)).split_multi()
         print ("writing vds to" + destination_path)
-        vcf.repartitions(number_partitions).write(destination_path,overwrite=True)
+        vcf.repartition(number_partitions).write(destination_path,overwrite=True)
         return True
     except ValueError:
         print (ValueError)

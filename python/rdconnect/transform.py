42d0218dd2e0c164307a075a3112ea602ad828ed
Davide Piscia <dpiscia@Davides-MacBook-Air.local>
2017-11-09 13:47:24 +0100

    intergenetic

--- python/rdconnect/transform.py
+++ python/rdconnect/transform.py
@@ -1,8 +1,7 @@
 def transform(dataset,destination,fileName):
     dataset.annotate_variants_expr([
         'va= let c= va in drop(va,info,rsid,qual,filters)',
-        'va.vep = let c= va.vep in drop(va.vep,colocated_variants,motif_feature_consequences,intergenic_consequences,regulatory_feature_consequences,most_severe_consequence,variant_class, assembly_name,allele_string,ancestral,context,end,id,input,seq_region_name,start,strand)',
-        'va.effs =  va.vep.transcript_consequences.map(x=>  {gene_name:  x.gene_symbol, effect_impact: x.impact ,transcript_id: x.transcript_id, effect : x.consequence_terms , gene_id : x.gene_id ,functional_class:  "transcript" , amino_acid_length : x.distance, codon_change :x.hgvsc, amino_acid_change : x.hgvsp, exon_rank: x.exon, transcript_biotype: x.biotype, gene_coding: str(x.cds_start)+"/"+str(x.cds_end)})',
+        'va.effs1 =  va.vep.transcript_consequences.map(x=>  {gene_name:  x.gene_symbol, effect_impact: x.impact ,transcript_id: x.transcript_id, effect : x.consequence_terms , gene_id : x.gene_id ,functional_class:  "transcript" , amino_acid_length : x.distance, codon_change :x.hgvsc, amino_acid_change : x.hgvsp, exon_rank: x.exon, transcript_biotype: x.biotype, gene_coding: str(x.cds_start)+"/"+str(x.cds_end)})',
         'va.vep.transcript_consequences =  va.vep.transcript_consequences.map(x=> {(let vaf = x in drop(x,biotype,uniparc))})',
         'va.samples = gs.filter(x=> x.dp >7 && x.gq> 19).map(g=>  {gq: g.gq, dp : g.dp, gt:intToGenotype(g.gt) , gtInt : g.gt,adBug : g.ad, ad : if(g.gt >0) truncateAt(g.ad[1]/g.ad.sum.toFloat,2) else truncateAt(g.ad[0]/g.ad.sum.toFloat,2), sample : s}  ).collect()',
         'va.chrom=  v.contig',
@@ -12,6 +11,8 @@ def transform(dataset,destination,fileName):
         'va.indel =  if ( (v.ref.length !=  v.altAlleles.map(x=> x.ref)[0].length) || (v.ref.length !=1) ||  ( v.altAlleles.map(x=> x.ref)[0].length !=1))  true else false'
     ]).annotate_variants_expr('va.af = va.samples.map(x=> x.gtInt).sum()/va.samples.filter(x=> x.dp > 8).map(x=> 2).sum()'
                               ).annotate_variants_expr(['''va.populations = [{
+                                      va.vep = let c= va.vep in drop(va.vep,colocated_variants,motif_feature_consequences,intergenic_consequences,regulatory_feature_consequences,most_severe_consequence,variant_class, assembly_name,allele_string,ancestral,context,end,id,input,seq_region_name,start,strand),
+                                      va.eff= va.vep.intergenic_consequences.map( x=> va.effs1.append({gene_name: "", effect_impact: x.impact ,transcript_id: "", effect : x.consequence_terms , gene_id : "" ,functional_class:  "intergenic" , amino_acid_length : 0, codon_change :"", amino_acid_change : "", exon_rank: "", transcript_biotype: "", gene_coding: ""})),
                                       af_internal:va.af , exac : removedot(va.dbnsfp.ExAC_AF,4)   ,
                                       gp1_asn_af : removedot(va.dbnsfp.Gp1_ASN_AF1000,4), gp1_eur_af: removedot(va.dbnsfp.Gp1_EUR_AF1000,4),
                                       gp1_af: removedot(va.dbnsfp.Gp1_AFR_AF1000,4) ,

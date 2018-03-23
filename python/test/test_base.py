082988c1b9513196e33be5862bc17bf5d92804e6
Cristina Luengo <cristinaluengoagullo@gmail.com>
2018-03-23 12:07:31 +0100

    Configuration changes in tests for Jenkins

--- python/test/test_base.py
+++ python/test/test_base.py
@@ -8,16 +8,18 @@ class BaseTestClass(unittest.TestCase):
 
     def setUp(self):
         """ Creates Spark and Hail contexts and gets configuration values """
+        self.config = config.readConfig("config.json")
         self.spark = SparkSession.builder \
-                                 .master("local") \
+                                 .master("local[*]") \
                                  .appName("vcfLoader unit tests") \
                                  .config("spark.sql.files.openCostInBytes","1099511627776") \
                                  .config("spark.sql.files.maxPartitionBytes","1099511627776") \
+                                 .config("spark.driver.extraClassPath",self.config["hailJarPath"]) \
+                                 .config("spark.driver.extraJavaOptions","-Dorg.xerial.snappy.tempdir=" + self.config["tmpDir"]) \
                                  .getOrCreate()
         self.sc = self.spark.sparkContext
         self.hc = HailContext(self.sc)
         self.sqlContext = SQLContext(self.sc)
-        self.config = config.readConfig("config.json")
 
     def tearDown(self):
         """ Stops Spark and Hail contexts """

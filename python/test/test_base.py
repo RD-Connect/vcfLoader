2161188ee3a726ef43b95c1b855c97388927a738
Cristina Luengo <cristinaluengoagullo@gmail.com>
2018-04-16 10:50:06 +0200

    Tests generalization and dbSNP unit test addition

--- python/test/test_base.py
+++ python/test/test_base.py
@@ -2,8 +2,18 @@ import unittest
 from rdconnect import config
 from hail import HailContext
 from pyspark.sql import SparkSession, SQLContext
+import shutil
+import nose.tools
 
-""" Base class for vcfLoader unit tests """
+""" 
+Base class for vcfLoader unit tests.
+This class defines the common setup for all tests and implements the general test function.
+Every specific test inherits from this class and defines the configuration it needs in order
+to run the test (e.g. sample path, results path, etc). Therefore, the base class is marked
+as 'nottest', since the test function will not be ran until the specific configuration of the 
+test that is running is specified.
+"""
+@nose.tools.nottest
 class BaseTestClass(unittest.TestCase):
 
     def setUp(self):
@@ -21,8 +31,27 @@ class BaseTestClass(unittest.TestCase):
         self.hc = HailContext(self.sc)
         self.sqlContext = SQLContext(self.sc)
 
-    def tearDown(self):
-        """ Stops Spark and Hail contexts """
+    def test(self):
+        """ Runs a general test with specific parameters from each test that inherits from this class. """
+        """ The test reads the annotated table and compares it to the expected one (both in KeyTable format) """
+        # Reading annotated VDS and parsing it into a KeyTable. The steps to follow are:
+        # - Get variants table
+        # - Expand variant types (otherwise we just get the 'v' type, instead of the chrom, position, etc)
+        # - Flatten struct types (so we can select the columns with 'va._'. Otherwise, we can just access 'va')
+        # - Select specified columns and key by specified key (e.g. v.start)
+        annotated_table = self.hc.read(self.sample_path) \
+                                 .variants_table() \
+                                 .expand_types() \
+                                 .flatten() \
+                                 .select(self.columns).order_by(self.key).key_by(self.key)
+        expected_table = self.hc.import_table(self.results_path, types=self.types).key_by(self.key)
+        self.assertTrue(annotated_table.same(expected_table))
+        
+    def tearDown(self, tmp_dirs):
+        """ Stopping Spark and Hail contexts """
         self.sc.stop()
         self.hc.stop()
         self.spark.stop()
+        # Removing temporal directories (specified by each class that inherits from this one) 
+        for directory in tmp_dirs:
+            shutil.rmtree(directory)
--- python/test/test_clinvar_annotations.py
+++ python/test/test_clinvar_annotations.py
@@ -1,68 +1,44 @@
 from rdconnect import annotations
 from test_base import BaseTestClass
+from hail import expr
 from pyspark.sql.functions import col, size
 import shutil
+import nose.tools
 
-""" Class for testing clinvar annotations """
+""" 
+Class for testing clinvar annotations
+It sets up all the necessary configuration in order to run the general test
+defined in 'BaseTestClass'. We specify the 'nose.tools.istest' notation
+so it can automatically detect the test function in 'BaseTestClass'.
+"""
+@nose.tools.istest
 class ClinvarAnnotationsTests(BaseTestClass):
 
     def setUp(self):
-        """ Prepares the annotation of sample variants with Clinvar annotations before the tests """
-        # Calling the parent function
+        """ Sets up the necessary configuration to run the test. """
+        print("\n------- Setting up the configuration for clinvar test -------\n")
+        # Calling the parent setUp function 
         super(ClinvarAnnotationsTests,self).setUp()
-        # Import variants from vcf
+        # Importing variants from vcf
         variants = self.hc.import_vcf(self.config["sampleVcfPath"])
-        # Write temporal directory to store the sample vcf variant dataset
-        self.hc.import_vcf(self.config["clinvarVcfPath"]).write(self.config["clinvarVdsPath"])
-        # Annotate variants with Clinvar
+        # Writing temporal directory to store the sample vcf variant dataset
+        self.hc.import_vcf(self.config["clinvarVcfPath"]).write(self.config["clinvarVdsPath"],overwrite=True)
+        # Creating annotated variants with Clinvar
         annotations.annotateClinvar(self.hc,variants,self.config["clinvarVdsPath"],self.config["sampleVdsPath"])
-        self.annotatedDf = self.hc.read(self.config["sampleVdsPath"]).variants_table().to_dataframe().sort("`v.start`")
-
-    def test_clnsig_values(self):
-        """ Tests whether clinical significance values in the clinvar annotation file
-        are mapped to their corresponding ids for display """
-	# Creation of expected results dataframe 
-        expected_data = self.sc.parallelize([[94018,'5|4'],[94026,'5|4|6'],[94545,'6|255'],[282897,'0|6'],
-                                           [292927,'0|255'],[294953,'C|6'], [323006,'C|255'],[329188,'4'],
-                                           [412449,'5'],[416988,'2|2'],[429977,'5|255|255'],[435880,'3'],[459011,'5|4']])
-        expected_df = self.spark.createDataFrame(expected_data, ["v.start","va.clinvar_clnsig"])
-        # Select only position and clinical significance columns, sorted by position
-        results = self.annotatedDf.select("`v.start`","`va.clinvar_clnsig`").filter("`va.clinvar_clnsig` != 'None'").distinct().collect()
-        expected_results = expected_df.collect()
-        self.assertTrue(results == expected_results)
-
-    def test_clnsig_filter_values(self):
-        """ Tests whether clinical significance values in the clinvar annotation file
-        are mapped to their corresponding ids for filtering """
-	# Creation of expected results rdd 
-        expected_data = self.sc.parallelize([['5','4'],['5','4','6'],['6','255'],['0','6'],
-                                           ['0','255'],['C','6'], ['C','255'],['4'],['5'],
-                                           ['2','2'],['5','255','255'],['3'],['5','4']])
-        # Select only clinical significance filter columns, sorted by position
-        results = self.annotatedDf.select("`va.clinvar_filter`") \
-                                .filter(size(col("`va.clinvar_filter`")) > 0) \
-                                .rdd \
-                                .map(lambda x: [y[0] for y in x[0]]) \
-                                .collect()
-        expected_results = expected_data.collect()
-        self.assertTrue(results == expected_results)
-
-    def test_id_values(self):
-        """ Tests whether clinvar ids are correctly annotated """
-        # Creation of expected results dataframe 
-        expected_data = self.sc.parallelize([[94018,'475283'],[94026,'183381'],[94545,'475278'],[282897,'402986'],
-                                           [292927,'161455'],[294953,'161454'], [323006,'475281'],[329188,'475282'],
-                                           [412449,'474165'],[416988,'474166'],[429977,'429977'],[435880,'127198'],[459011,'429977']])
-        expected_df = self.spark.createDataFrame(expected_data, ["v.start","va.clinvar_id"])
-        # Select only position and clinical id columns, sorted by position
-        results = self.annotatedDf.select("`v.start`","`va.clinvar_id`").filter("`va.clinvar_id` != 'None'").distinct().collect()
-        expected_results = expected_df.collect()
-        self.assertTrue(results == expected_results)
+        # Defining specific configuration values for the test
+        self.sample_path = self.config["sampleVdsPath"]
+        self.results_path = self.config["clinvarTable"]
+        self.columns = ["v.contig", "v.start", "va.clinvar_id", "va.clinvar_clnsig", "va.clinvar_filter"]
+        # Types for table schema
+        self.types = { 'v.contig': expr.TString(),
+                  'v.start': expr.TInt(),
+                  'va.clinvar_clnsig': expr.TString(),
+                  'va.clinvar_filter': expr.TArray(expr.TStruct(["clnsig"],[expr.TString()])),
+                  'va.clinvar_id': expr.TString() }
+        self.key = "v.start"
         
     def tearDown(self):
         """ Removes temporal directories once the tests are done """
         # Calling the parent function
-        super(ClinvarAnnotationsTests,self).tearDown()
-        # Remove temporal directories
-        shutil.rmtree(self.config["clinvarVdsPath"])
-        shutil.rmtree(self.config["sampleVdsPath"])
+        dirs = [self.config["clinvarVdsPath"], self.config["sampleVdsPath"]]
+        super(ClinvarAnnotationsTests,self).tearDown(dirs)

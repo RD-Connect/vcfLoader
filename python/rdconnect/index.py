accc6c60d330f954224f2fa32a59ce176c2f6b6b
Cristina Luengo <cristinaluengoagullo@gmail.com>
2018-06-18 09:37:55 +0200

    freqInt type change

--- python/rdconnect/index.py
+++ python/rdconnect/index.py
@@ -13,7 +13,7 @@ def create_index(host,port,index_name,version):
                 ,"ref":{"type":"keyword","index":"false"}
                 ,"alt":{"type":"keyword","index":"false"}
                 ,"indel":{"type":"keyword"}
-                ,"freqInt":{"type":"float","index":"true"}
+                ,"freqInt":{"type":"keyword"}
                 ,"rs":{"type":"keyword"}
                 ,"cadd_phred":{"type":"float","index":"true"}
                 ,"gerp_rs":{"type":"keyword","index":"false"}

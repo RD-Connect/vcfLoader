686ca54808956c9124ecb5a8884fe0a3e5a9b471
Cristina Luengo <cristinaluengoagullo@gmail.com>
2018-07-06 17:12:29 +0200

    Cadd base test

--- python/test/test_cadd_annotations.py
+++ python/test/test_cadd_annotations.py
@@ -6,46 +6,39 @@ import shutil
 import nose.tools
 
 """ 
-Class for testing dbNSFP annotations
+Class for testing CADD annotations
 It sets up all the necessary configuration in order to run the general test
 defined in 'BaseTestClass'. We specify the 'nose.tools.istest' notation
 so it can automatically detect the test function in 'BaseTestClass'.
 """
 @nose.tools.istest
-class DbNSFPAnnotationsTests(BaseTestClass):
+class CaddAnnotationsTests(BaseTestClass):
 
     def setUp(self):
         """ Sets up the necessary configuration to run the test. """
-        print("\n------- Setting up the configuration for dbNSFP test -------\n")
+        print("\n------- Setting up the configuration for CADD test -------\n")
         # Calling the parent setUp function 
-        super(DbNSFPAnnotationsTests,self).setUp()
+        super(CADDAnnotationsTests,self).setUp()
         # Importing variants from vcf
         variants = self.hc.import_vcf(self.config["sampleVcfPath"]).split_multi()
         # Writing temporal directory to store the sample vcf variant dataset
-        self.hc.import_vcf(self.config["dbNSFPTablePath"]).split_multi().write(self.config["dbNSFPVdsPath"],overwrite=True)
+        self.hc.import_vcf(self.config["caddVcfPath"]).split_multi().write(self.config["caddVdsPath"],overwrite=True)
         # Creating annotated variants with Clinvar
-        annotations.annotateDbNSFP(self.hc,variants,self.config["dbNSFPVdsPath"],self.config["sampleVdsPath"])
+        annotations.annotateDbNSFP(self.hc,variants,self.config["caddVdsPath"],self.config["sampleVdsPath"])
         # Defining specific configuration values for the test
         self.sample_path = self.config["sampleVdsPath"]
-        self.results_path = self.config["dbNSFPTable"]
-        self.columns = ["v.contig", "v.start", "va.gerp_rs", "va.mt", "va.mutationtaster_pred", "phylop46way_placental", "polyphen2_hvar_pred", "polyphen2_hvar_score", "sift_pred", "sift_score"]
+        self.results_path = self.config["caddTable"]
+        self.columns = ["v.contig", "v.start", "va.cadd_phred"]
         # Types for table schema
         self.types = { 'v.contig': expr.TString(),
                   'v.start': expr.TInt(),
-                  'va.gerp_rs': expr.TString(),
-                  'va.mt': expr.TDouble(),
-                  'va.mutationtaster_pred': expr.TString(),
-                  'phylop46way_placental': expr.TString(),
-                  'polyphen2_hvar_pred': expr.TString(),
-                  'polyphen2_hvar_score': expr.TDouble(),
-                  'sift_pred': expr.TString(),
-                  'sift_score': expr.TDouble()
+                  'va.cadd_phred': expr.TString()
         }
         self.key = "v.start"
-        self.posRange = self.config["dbNSFPRange"]
+        self.posRange = self.config["caddRange"]
         
     def tearDown(self):
         """ Removes temporal directories once the tests are done """
         # Calling the parent function
-        dirs = [self.config["dbNSFPVdsPath"], self.config["sampleVdsPath"]]
+        dirs = [self.config["caddVdsPath"], self.config["sampleVdsPath"]]
         super(DbNSFPAnnotationsTests,self).tearDown(dirs)

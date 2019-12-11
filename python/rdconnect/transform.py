9b031b688e2c58e314c87d311a06081e4b544f0d
Carles Hernandez-Ferrer <carleshf@users.noreply.github.com>
2019-12-11 17:46:55 +0100

    Update transform.py

--- python/rdconnect/transform.py
+++ python/rdconnect/transform.py
@@ -7,8 +7,8 @@ def transform(dataset, destinationPath, chrom):
           :param String destinationPath: Path where the loaded annotation table will be put
           :param Int chrom: Chromosome number
     """
-    cponsole('[in 1] ' + destinationPath)
-    cponsole('[in 2] ' + destinationPath + "/chrom=" + chrom)
+    print('[in 1] ' + destinationPath)
+    print('[in 2] ' + destinationPath + "/chrom=" + chrom)
     dataset.to_spark() \
            .drop("locus.contig", "locus.position", "alleles") \
            .write.mode('overwrite').save(destinationPath + "/chrom=" + chrom)

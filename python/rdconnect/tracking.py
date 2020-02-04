607360a943b2d936f19966d9c9119f4b86f1e651
Carles Hernandez-Ferrer <carleshf@users.noreply.github.com>
2020-02-04 17:56:10 +0100

    Cleaned #

--- python/rdconnect/tracking.py
+++ python/rdconnect/tracking.py
@@ -26,21 +26,21 @@ def update_data_last_index(host, port, num_shards, num_replicas, user, pwd, data
 	url = "http://{}:{}/data_tracking/1.0.0/_search".format(host, port)
 	headers = {'Content-Type': 'application/json'}
 	data = "{ \"query\": { \"term\": { \"platform\": \"" + data_project + "\" } } }"
-	response = requests.get(url, data = data, headers = headers) #, auth = (user,pwd))
+	response = requests.get(url, data = data, headers = headers, auth = (user,pwd))
 	cnt = json.loads(response.text)['hits']
 
 	# If no last entry
 	if response.status_code == 200 and cnt['total'] == 0:
 		url = "http://{}:{}/data_tracking/1.0.0/".format(host, port)
 		data = "{ \"platform\": \"" + data_project + "\", \"index\": \"" + data_index + "\" }"
-		response = requests.post(url, data = data, headers = headers) #, auth = (user,pwd))
+		response = requests.post(url, data = data, headers = headers, auth = (user,pwd))
 		if response.status_code != 201:
 			raise Exception('Obtained status code "{}" when inserting content.'.format(response.status_code))	
-		# If last entry
+	# If last entry
 	elif response.status_code == 200 and cnt['total'] == 1:
 		url = "http://{}:{}/data_tracking/1.0.0/{}".format(host, port, cnt['hits'][0]['_id'])
 		data = "{ \"platform\": \"" + data_project + "\", \"index\": \"" + data_index2 + "\" }"
-		response = requests.post(url, data = data, headers = headers) #, auth = (user,pwd))
+		response = requests.post(url, data = data, headers = headers, auth = (user,pwd))
 		if response.status_code != 200:
 			raise Exception('Obtained status code "{}" when updating content.'.format(response.status_code))	
 	# If strange situations happens

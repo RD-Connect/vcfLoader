4508a25a6bbc6951231226deec8e8d3438a42db1
Carles Hernandez-Ferrer <carleshf@users.noreply.github.com>
2020-02-04 17:28:50 +0100

    Update tracking.py
    Added code to insert/update the index of a given project.

--- python/rdconnect/tracking.py
+++ python/rdconnect/tracking.py
@@ -9,8 +9,7 @@ def update_data_last_index(host, port, num_shards, num_replicas, user, pwd, data
 			"settings": { 
 				"index": { 
 					"number_of_shards": {}, 
-					"number_of_replicas": {}, 
-					"refresh_interval": "-1" 
+					"number_of_replicas": {}
 				} 
 			}, 
 			"mappings":{ "1.0.0" :{
@@ -22,4 +21,31 @@ def update_data_last_index(host, port, num_shards, num_replicas, user, pwd, data
 		index.create_index(host, port, 'data_tracking', data, user, pwd)
 	else:
 		print('[INFO]: Tracking index does exists. No need to create it.')
+
+	# Get last entry
+	url = "http://{}:{}/data_tracking/1.0.0/_search".format(host, port)
+	headers = {'Content-Type': 'application/json'}
+	data = "{ \"query\": { \"term\": { \"platform\": \"" + data_project + "\" } } }"
+	response = requests.get(url, data = data, headers = headers) #, auth = (user,pwd))
+	cnt = json.loads(response.text)['hits']
+
+	# If no last entry
+	if response.status_code == 200 and cnt['total'] == 0:
+		url = "http://{}:{}/data_tracking/1.0.0/".format(host, port)
+		data = "{ \"platform\": \"" + data_project + "\", \"index\": \"" + data_index + "\" }"
+		response = requests.post(url, data = data, headers = headers) #, auth = (user,pwd))
+		if response.status_code != 201:
+			raise Exception('Obtained status code "{}" when inserting content.'.format(response.status_code))	
+		# If last entry
+	elif response.status_code == 200 and cnt['total'] == 1:
+		url = "http://{}:{}/data_tracking/1.0.0/{}".format(host, port, cnt['hits'][0]['_id'])
+		data = "{ \"platform\": \"" + data_project + "\", \"index\": \"" + data_index2 + "\" }"
+		response = requests.post(url, data = data, headers = headers) #, auth = (user,pwd))
+		if response.status_code != 200:
+			raise Exception('Obtained status code "{}" when updating content.'.format(response.status_code))	
+	# If strange situations happens
+	elif response.status_code == 200 and cnt['total'] > 1:
+		raise Exception('Duplicated entries in tracking index are not allowed! Clean the index to proceed.')
+	else:
+		raise Exception('Obtained status code "{}"'.format(response.status_code))
 

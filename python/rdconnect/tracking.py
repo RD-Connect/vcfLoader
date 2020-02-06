830fc79822ecb6645c40a8ae6336e897a2d37a8b
Carles Hernandez-Ferrer <carleshf@users.noreply.github.com>
2020-02-06 11:20:18 +0100

    Added new info to display

--- python/rdconnect/tracking.py
+++ python/rdconnect/tracking.py
@@ -6,11 +6,11 @@ from rdconnect import index
 
 def update_data_last_index(host, port, num_shards, num_replicas, user, pwd, data_project, data_index): 
 	if not index.index_exists(host, port, "data_tracking", user, pwd):
-		print('[INFO]: Tracking index does not exists. Its creation proceeds.')
+		print('[INFO]:   . Tracking index does not exists. Its creation proceeds.')
 		data = "{\"settings\": { \"index\": { \"number_of_shards\": \"" + num_shards + "\", \"number_of_replicas\": \"" + num_replicas + "\" } }, \"mappings\":{ \"1.0.0\" :{ \"properties\": { \"platform\" : { \"type\" : \"text\", \"index\": \"true\" }, \"index\" : { \"type\" : \"text\" } } } } }"
 		index.create_index(host, port, 'data_tracking', data, user, pwd)
 	else:
-		print('[INFO]: Tracking index does exists. No need to create it.')
+		print('[INFO]:   . Tracking index does exists. No need to create it.')
 
 	# Get last entry
 	url = "http://{}:{}/data_tracking/1.0.0/_search".format(host, port)
@@ -46,8 +46,10 @@ def update_samples_data_management(initial_vcf, data_url, data_token):
 	vcf = hl.split_multi_hts(hl.import_vcf(str(initial_vcf), array_elements_required = False, force_bgz = True, min_partitions = 2))
 	full_samples = [y.get('s') for y in vcf.col.collect()]
 
-	print ("[INFO]:   . Experiments in loaded VCF: {}".format(len(full_samples)))
-	print ("[INFO]:   . First and last sample: {} // {}".format(full_samples[0], full_samples[len(full_samples) - 1]))
+	print('[INFO]:   . Experiments in loaded VCF: {}'.format(len(full_samples)))
+	print('[INFO]:   . First and last sample: {} // {}'.format(full_samples[0], full_samples[len(full_samples) - 1]))
+	print('[INFO]:   . Provided URL for data-management: {}'.format(data_url))
+	print('[INFO]:   . Provided token for data-management: {}'.format(data_token))
 
 	for sam in full_samples:
 		response = requests.post(data_url + sam, headers = headers)

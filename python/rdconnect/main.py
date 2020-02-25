606ad8644472af6e880f4adeabe70dc7c2215f46
dpiscia <davidepi79@gmail.com>
2020-02-25 08:24:28 +0100

    100 samples

--- python/rdconnect/main.py
+++ python/rdconnect/main.py
@@ -106,7 +106,7 @@ def main(sqlContext, configuration, chrom, nchroms, step, somaticFlag):
             prefix_hdfs=configuration["combine"]["prefix_hdfs"]
             sourceFilesName=combine.get_experiment_by_group(group,url_project,token,prefix_hdfs,chrom)
             print(sourceFilesName[0])
-            combine.load_gvcf(hl, sourceFilesName, chrom, new_gvcf_store_path+"/chrom-"+chrom, gvcf_store_path+"/chrom-"+chrom)
+            combine.load_gvcf(hl, sourceFilesName, chrom, new_gvcf_store_path+"/chrom-"+chrom, gvcf_store_path)
 
     # Pipeline steps
         

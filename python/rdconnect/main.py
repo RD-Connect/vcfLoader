d43dd68e6af2704945d6283c5eab03a8b1f7e8fc
Carles Hernandez <carles.hernandez@cnag.crg.eu>
2020-03-13 14:22:31 +0100

    ..

--- python/rdconnect/main.py
+++ python/rdconnect/main.py
@@ -110,7 +110,7 @@ def main(sqlContext, configuration, chrom, nchroms, step, somaticFlag):
             url_project = configuration[ 'datamanagement' ][ 'host' ]
             group = configuration[ 'combine' ][ 'group' ]
             prefix_hdfs = configuration[ 'combine' ][ 'prefix_hdfs' ]
-            combine.createSparseMatrix( group, url_project, token, prefix_hdfs, chrom, max_items_batch ):
+            combine.createSparseMatrix( group, url_project, token, prefix_hdfs, chrom, max_items_batch )
 
 
     if ("createDenseMatrix" in step):

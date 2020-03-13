347c65150ba837faf0d0523825e3947870adf2af
Carles Hernandez <carles.hernandez@cnag.crg.eu>
2020-03-13 16:29:11 +0100

    ..

--- python/rdconnect/combine.py
+++ python/rdconnect/combine.py
@@ -55,7 +55,8 @@ def createSparseMatrix( group, url_project, token, prefix_hdfs, chrom, max_items
     for index, batch in enumerate( batches ):
         print( index )
         if index == 0:
-            gvcf_store_path = gvcf_store_path
+            gvcf_store_path = '{0}/chrom-{1}'.format( gvcf_store_path, chrom )
+            print( "current gvcf store is ------> " + gvcf_store_path )
         else:
             gvcf_store_path = '{0}/chrom-{1}'.format( new_gvcf_store_path, chrom )
             new_gvcf_store_path = '{0}/chrom-{1}'.format( utils.update_version( new_gvcf_store_path ), chrom )
@@ -208,7 +209,6 @@ def loadGvcf( hl, files, chrom, destinationPath, gvcfStorePath, partitions ):
 
     if gvcfStorePath == None:
         comb = combine_gvcfs( vcfs )
-        
     else:
         gvcf_store = hl.read_matrix_table( gvcfStorePath )
         comb = combine_gvcfs( [ gvcf_store ] + vcfs )

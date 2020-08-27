54641a493716632d2809de4a312c5d55807aebb6
Carles Hernandez <2046180+carleshf@users.noreply.github.com>
2020-08-27 14:48:15 +0200

    ..

--- python/rdconnect/transform.py
+++ python/rdconnect/transform.py
@@ -1,7 +1,7 @@
 
 from rdconnect.expr import annotationsExprs
 
-def transform(dataset, destinationPath, chrom):
+def transform(hl, dataset, destinationPath, chrom):
     """ Transforms a given dataset into the dataframe format for ElasticSearch
           :param VariantDataSet dataset: Dataset to transform
           :param String destinationPath: Path where the loaded annotation table will be put
